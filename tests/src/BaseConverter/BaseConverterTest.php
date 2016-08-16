17caa17014ebf5882f16dc7da3b8781ce34745f9
Francesco Zaffaroni <fraz.93@gmail.com>
2016-08-16 23:08:47 +0200

    Added first tests

---
+++ tests/src/BaseConverter/BaseConverterTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Fzaffa\BaseConverter;
+
+use PHPUnit\Framework\TestCase;
+
+class BaseConverterTest extends TestCase{
+
+    private $converter;
+
+    public function setUp()
+    {
+        $this->converter = new BaseConverter(ConverterRangeTypes::azAZ);
+    }
+
+    public function testAutomaticConvertType()
+    {
+        $this->assertInternalType("int", $this->converter->convert("mnS"));
+
+        $this->assertInternalType("string", $first = $this->converter->convert("1234"));
+        $this->assertInternalType("string", $second = $this->converter->convert(1234));
+
+        $this->assertEquals($second, $first);
+    }
+}
