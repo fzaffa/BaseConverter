86e39a03a753c13426c7c7e55d572e971ce928de
Francesco Zaffaroni <fraz.93@gmail.com>
2016-08-27 19:18:28 +0200

    Added docblocks and PSR-2 compliance

--- src/BaseConverter/BaseConverter.php
+++ src/BaseConverter/BaseConverter.php
@@ -1,76 +1,125 @@
 <?php
+/**
+ * Fzaffa/BaseConverter
+ *
+ *
+ * @version 0.1-dev
+ *
+ * @author      Francesco Zaffaroni
+ * @link        http://github.com/fzaffa/baseConverter
+ * @copyright   Copyright (c) 2016 Francesco Zaffaroni
+ * @license     https://opensource.org/licenses/MIT
+ *
+ */
 namespace Fzaffa\BaseConverter;
 
-class BaseConverter {
-	
-	private $letters;
-	private $base;
+/**
+ * Class BaseConverter
+ * @package Fzaffa\BaseConverter
+ */
 
-	public function __construct($letters)
-	{
-		if(!is_array($letters))
-		{
-			switch($letters){
-				case "azAZ":
-					$letters = array_merge(range("a", "z"), range("A", "Z"));
-					break;
-				case "az":
-					$letters = range("a", "z");
-					break;
-				case "AZ":
-					$letters = range("A", "Z");
-					break;
-				default:
-					throw new \Exception("Provided parameter must be an array of characters or an option.");
-					break;
-			}
-		}
-		$this->letters = $letters;
-		$this->base = count($letters);
-	}
+class BaseConverter
+{
 
-	public function convert($arg)
-	{
-		if(is_numeric($arg))
-		{
-			return $this->convertFromIntToString($arg);
-		} elseif(is_string($arg)) {
-			return $this->convertFromStringToInt($arg);
-		}
-		throw new \Exception("Argument passed must be string or numeric", 1);
-	}
+    /**
+     * The base, or length of the alphabet.
+     *
+     * @var int
+     */
+    protected $base;
+    /**
+     * The alphabet used for the conversion.
+     *
+     * @var array
+     */
+    protected $letters;
 
-	public function convertFromIntToString($int)
-	{
-		if($int < count($this->letters))
-		{
-			return $this->letters[$int];
-		}
-		$returnValue = '';
+    /**
+     * BaseConverter constructor.
+     *
+     * @param $letters can be an option or an array. Used to build the letters property.
+     * @throws \Exception if the provided argument isn't a valid option or an array of characters.
+     */
+    public function __construct($letters)
+    {
+        if (!is_array($letters)) {
+            switch ($letters) {
+                case ConverterRangeTypes::ALPHA_ULCASE:
+                    $letters = array_merge(range("a", "z"), range("A", "Z"));
+                    break;
+                case ConverterRangeTypes::ALPHA_LCASE:
+                    $letters = range("a", "z");
+                    break;
+                case ConverterRangeTypes::ALPHA_UCASE:
+                    $letters = range("A", "Z");
+                    break;
+                default:
+                    throw new \Exception("Provided parameter must be an array of characters or an option.");
+                    break;
+            }
+        }
+        $this->letters = $letters;
+        $this->base = count($letters);
+    }
 
-		while($int != 0)
-		{
-			$returnValue = $this->letters[bcmod($int, $this->base)].$returnValue;
-			$int = bcdiv($int, $this->base, 0);
-		}
+    /**
+     * Automatically understands if the provided argument is a string or a number and acts accordingly.
+     *
+     * @param $arg string or numeric, the value to be converted.
+     * @return int|string returns the converted value.
+     * @throws \Exception if the argument isn't a string or isn't numeric.
+     */
+    public function convert($arg)
+    {
+        if (is_numeric($arg)) {
+            return $this->convertFromIntToString($arg);
+        } elseif (is_string($arg)) {
+            return $this->convertFromStringToInt($arg);
+        }
+        throw new \Exception("Argument passed must be string or numeric", 1);
+    }
 
-		return $returnValue;
-	}
+    /**
+     * Only converts an integer to a string.
+     *
+     * @param $int The number to be converted in string
+     * @return string the converted value
+     */
+    public function convertFromIntToString($int)
+    {
+        if ($int < count($this->letters)) {
+            return $this->letters[$int];
+        }
+        $returnValue = '';
 
-	public function convertFromStringToInt($string)
-	{
-		$lettersArray = str_split($string);
-		$lettersArray = array_reverse($lettersArray);
-		$number = 0;
-		$index = 0;
-		foreach ($lettersArray as $letter){
-			$num = array_search($letter, $this->letters);
-			$num = $num * pow($this->base, $index);
-			$number += $num;
-			$index++;
-		}
+        while ($int != 0) {
+            $returnValue = $this->letters[bcmod($int, $this->base)] . $returnValue;
+            $int = bcdiv($int, $this->base, 0);
+        }
 
-		return $number;
-	}
+        return $returnValue;
+    }
+
+    /**
+     * Oly converts a string to a number.
+     *
+     * @param $string the previously converted string.
+     * @return int the integer returned.
+     */
+    public function convertFromStringToInt($string)
+    {
+        $lettersArray = str_split($string);
+        $lettersArray = array_reverse($lettersArray);
+        $number = 0;
+        $index = 0;
+        foreach ($lettersArray as $letter) {
+            $num = array_search($letter, $this->letters);
+            $num = $num * pow($this->base, $index);
+            $number += $num;
+            $index++;
+        }
+
+        return $number;
+    }
 }
 
--- src/BaseConverter/ConverterRangeTypes.php
+++ src/BaseConverter/ConverterRangeTypes.php
@@ -1,11 +1,34 @@
 <?php
-
-
+/**
+ * Fzaffa/BaseConverter
+ *
+ *
+ * @version     GIT: $Id$
+ *
+ * @author      Francesco Zaffaroni
+ * @link        http://github.com/fzaffa/baseConverter
+ * @copyright   Copyright (c) 2016 Francesco Zaffaroni
+ * @license     https://opensource.org/licenses/MIT
+ *
+ */
 namespace Fzaffa\BaseConverter;
 
-
-class ConverterRangeTypes {
-    const ALPHA_ULCASE = "azAZ";
-    const ALPHA_LCASE = "az";
-    const ALPHA_UCASE = "AZ";
+/**
+ * Class ConverterRangeTypes
+ * @package Fzaffa\BaseConverter
+ */
+class ConverterRangeTypes
+{
+    /**
+     * The set of characters [a-zA-Z]
+     */
+    const ALPHA_ULCASE = 1;
+    /**
+     * The set of characters [a-z]
+     */
+    const ALPHA_LCASE = 2;
+    /**
+     * The set of characters [A-Z]
+     */
+    const ALPHA_UCASE = 3;
 }
